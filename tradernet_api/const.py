27c959a486d30c14f669c1da107c4db6c7b61767
Artur Kutsevol <arthur.kutsevol@gmail.com>
2022-08-28 10:54:27 +0300

    Remove hardcoded string which are related to name of commands. Use the Command Enum class with values for each command name.

--- tradernet_api/const.py
+++ tradernet_api/const.py
@@ -4,11 +4,11 @@ api_url = "https://tradernet.ru/api"
 
 
 class Command(Enum):
-    send_order = "send_order"
-    delete_order = "delete_order"
-    get_orders = "get_orders"
-    get_ticker_info = "get_ticker_info"
-    set_stop_order = "set_stop_order"
+    send_order = "putTradeOrder"
+    delete_order = "delTradeOrder"
+    get_orders = "getNotifyOrderJson"
+    get_ticker_info = "getSecurityInfo"
+    set_stop_order = "putStopLoss"
 
 
 class SendOrderField(Enum):
